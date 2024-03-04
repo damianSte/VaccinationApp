962ff485d8a4096b019d1b78738e6c4b03696ae0
damianSte <stepien.damian.29@gmail.com>
2024-03-04 14:37:26 +0100

    SignUpActivity validating User data, email and makeToast Function. SignInActivity validating User data from Firebase

--- app/src/main/java/com/example/vaccinationapp/SignInActivity.kt
+++ app/src/main/java/com/example/vaccinationapp/SignInActivity.kt
@@ -3,12 +3,19 @@ package com.example.vaccinationapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var goToSignUpButton: Button
     private lateinit var breakInButton: Button
+    private lateinit var inputEmail: EditText
+    private lateinit var inputPassword: EditText
+    private lateinit var logToApp: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +23,73 @@ class SignInActivity : AppCompatActivity() {
 
 
         goToSignUpButton = findViewById(R.id.go_to_sign_up_button)
+        breakInButton = findViewById(R.id.break_in_button)
+        inputEmail = findViewById(R.id.email_edit)
+        inputPassword = findViewById(R.id.password_edit)
+        logToApp = findViewById(R.id.sign_in_button)
+
+
+        goToSignInActivity()
+
+    }
+
+    // Replace SomeActivity with actualActivity
+//    private fun breakInNoLog(){
+//        breakInButton.setOnClickListener{
+//            val intent = Intent(this, SomeActivity::class.java)
+//            startActivity(intent)
+//        }
+//  }
+
+    private fun goToSignInActivity() {
         goToSignUpButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun validateLoginDetails(): Boolean {
+        // getting email and password trimming spaces
+        val email = inputEmail.text.toString().trim()
+        val password = inputPassword.text.toString().trim()
+
+        return when {
+            TextUtils.isEmpty(email) -> {
+                makeToast(resources.getString(R.string.wrong_log_in_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(password) -> {
+                makeToast(resources.getString(R.string.wrong_log_in_password), true)
+                false
+            }
+            else -> {
+               true
+            }
+        }
+    }
+
+    private fun logInRegisteredUser() {
+        // validating from validateLoginDetails()
+        if (validateLoginDetails()) {
+            val email = inputEmail.text.toString().trim()
+            val password = inputPassword.text.toString().trim()
+
+            // getting email and password from Firebase
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Go to some Activity !!
+                        finish()
+                    } else {
+                        makeToast(task.exception?.message.toString(), true)
+                    }
+                }
+        }
+    }
+
+    private fun makeToast(toast: String, errorMessage: Boolean) {
+        Toast.makeText(this@SignInActivity, toast, Toast.LENGTH_LONG).show()
     }
 
 }
--- app/src/main/java/com/example/vaccinationapp/SignUpActivity.kt
+++ app/src/main/java/com/example/vaccinationapp/SignUpActivity.kt
@@ -3,6 +3,8 @@ package com.example.vaccinationapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -20,6 +22,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var registerPassword: EditText
     private lateinit var repeatPassword: EditText
     private lateinit var registerName: EditText
+    private lateinit var registerButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +33,14 @@ class SignUpActivity : AppCompatActivity() {
         registerPassword = findViewById(R.id.password_edit)
         repeatPassword = findViewById(R.id.repeat_password_edit)
         registerName = findViewById(R.id.name_edit)
+        registerButton = findViewById(R.id.sign_up_button)
 
         goToSignIn()
+        registerNewUserButton()
 
-        val registerButton = findViewById<Button>(R.id.sign_up_button)
+    }
+
+    private fun registerNewUserButton() {
         registerButton.setOnClickListener {
             registerUser()
         }
@@ -46,6 +53,49 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
+    private fun validateEmail(email: String): Boolean {
+        val emailPatter = Patterns.EMAIL_ADDRESS
+        return emailPatter.matcher(email).matches()
+    }
+
+    private fun registrationData(): Boolean {
+        val email = registerEmail.text.toString().trim()
+        val password = registerPassword.text.toString().trim()
+        val repeatPassword = repeatPassword.text.toString().trim()
+
+        return when {
+            TextUtils.isEmpty(email) -> {
+                makeToast(resources.getString(R.string.invalid_email), true)
+                false
+            }
+
+            !validateEmail(email) -> {
+                makeToast(resources.getString(R.string.invalid_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(password) -> {
+                makeToast(resources.getString(R.string.pass_regulation), true)
+                false
+            }
+
+            TextUtils.isEmpty(repeatPassword) -> {
+                if (repeatPassword != password) {
+                    makeToast(resources.getString(R.string.pass_not_the_same), true)
+                }
+                false
+            }
+
+            password != repeatPassword -> {
+                makeToast(resources.getString(R.string.pass_not_the_same), true)
+                false
+            }
+
+            else -> true
+        }
+    }
+
+
     private fun registerUser() {
         // email used for registration
         val email: String = registerEmail.text.toString().trim(' ')
@@ -56,30 +106,34 @@ class SignUpActivity : AppCompatActivity() {
         // name according to registered Name
         val name: String = registerName.text.toString().trim(' ')
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(
-                OnCompleteListener<AuthResult> { task ->
-                    if (task.isSuccessful) {
-                        val firebaseUser: FirebaseUser = task.result!!.user!!
+        if (registrationData()) {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+                        if (task.isSuccessful) {
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+                            makeToast("Registration successful, user Id ${firebaseUser.uid}", false)
 
-                        val user = User(
-                            "ID",
-                            name,
-                            true,
-                            email,
-                        )
-                        FireStoreClass().registerUserFS(this@SignUpActivity, user)
-                        // FirebaseAuth.getInstance().signOut() // Remove this line
-                        finish()
+                            val user = User(
+                                "ID",
+                                name,
+                                true,
+                                email,
+                            )
 
-                    } else {
-                        Toast.makeText(
-                            this@SignUpActivity, resources.getString(R.string.register_failed),
-                            Toast.LENGTH_LONG
-                        ).show()
+                            FireStoreClass().registerUserFS(this@SignUpActivity, user)
+                            // FirebaseAuth.getInstance().signOut() // Remove this line
+                            finish()
+
+                        } else {
+                            Toast.makeText(
+                                this@SignUpActivity, resources.getString(R.string.register_failed),
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
                     }
-                }
-            )
+                )
+        }
 
     }
 
@@ -89,4 +143,9 @@ class SignUpActivity : AppCompatActivity() {
             Toast.LENGTH_LONG
         ).show()
     }
+
+    private fun makeToast(toast: String, errorMessage: Boolean) {
+        Toast.makeText(this@SignUpActivity, toast, Toast.LENGTH_LONG).show()
+    }
+
 }
